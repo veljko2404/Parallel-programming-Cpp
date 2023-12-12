0654059c2ee22718d6a4351c336863bb4dda636d
Veljko Petkovic <38298277+veljko2404@users.noreply.github.com>
2023-12-12 05:34:40 +0100

    vezbe 5/2

--- 5_2/Source.cpp
+++ 5_2/Source.cpp
@@ -1,12 +1,98 @@
-#include<iostream>
-
+﻿#include<iostream>
+#include<omp.h>
+#include<chrono>
 using namespace std;
+using namespace std::chrono;
 
+typedef long long lng;
 
+void v1() {
+	lng num_steps = 1 << 20;
+	double step = 1.0 / num_steps;
+	double pi = 0;
+	int num_threads = 8;
+	auto start_t = high_resolution_clock::now();
+#pragma omp parallel num_threads(8) \
+shared(num_steps,step,pi,num_threads)
+	{
+		int tid = omp_get_thread_num();
+		int work_per_thread = (num_threads + num_steps - 1) / num_threads;
+		int start = tid * work_per_thread;
+		int end = tid == num_threads - 1 ? num_steps : (tid + 1) * work_per_thread;
+		double partial_sum = 0;
+		for (lng i = start; i < end; i++) {
+			double x = (i + 0.5) * step;
+			partial_sum += 4.0 / (1.0 + x * x);
+		}
+		partial_sum *= step;
+	#pragma omp atomic
+		pi += partial_sum;
+	}
+	auto end_t = high_resolution_clock::now();
+	auto micro_secs = duration_cast<microseconds>(end_t - start_t).count();
+	cout << micro_secs << ", " << pi << endl;
+}
+
+void v2() {
+	lng num_steps = 1 << 20;
+	double step = 1.0 / num_steps;
+	double pi = 0;
+	int num_threads = 8;
+	auto start_t = high_resolution_clock::now();
+#pragma omp parallel num_threads(num_threads) \
+shared(num_steps,step,pi,num_threads)
+	{
+		int tid = omp_get_thread_num();
+		double partial_sum = 0;
+		for (lng i = tid; i < num_steps; i += num_threads) {
+			double x = (i + 0.5) * step;
+			partial_sum += 4.0 / (1.0 + x * x);
+		}
+		partial_sum *= step;
+	#pragma omp atomic
+		pi += partial_sum;
+	}
+	auto end_t = high_resolution_clock::now();
+	cout << duration_cast<microseconds>(end_t - start_t).count() << ", " << pi << endl;;
+}
+
+void v3() {
+	lng num_steps = 1 << 20;
+	double step = 1.0 / num_steps;
+	double pi = 0;
+	auto start_t = high_resolution_clock::now();
+#pragma omp parallel num_threads(8) \
+shared(step,num_steps,pi)
+	{
+		double partial_sum = 0;
+	#pragma omp for
+		for (lng i = 0; i < num_steps; i++) {
+			double x = (i + 0.5) * step;
+			partial_sum += 4.0 / (1.0 + x * x);
+		}
+		partial_sum *= step;
+	#pragma omp atomic
+		pi += partial_sum;
+	}
+	auto end_t = high_resolution_clock::now();
+	cout << duration_cast<microseconds>(end_t - start_t).count() << ", " << pi;
+}
+
+void v4() {
+
+}
+
+void v5() {
+
+}
 
 int main() {
 
-
+	v1();
+	v2();
+	v3();
+	v4();
+	v5();
 
 	return 0;
 }
